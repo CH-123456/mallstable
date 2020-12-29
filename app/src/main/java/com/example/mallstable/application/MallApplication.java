190a6c79f6f164884906efef3cae702652abf4a5
ytu07111 <2889653326@qq.com>
2020-12-29 10:55:15 +0800

    88

---
+++ app/src/main/java/com/example/mallstable/application/MallApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallstable.application;
+
+import android.app.Application;
+
+public class MallApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    private void initOkHttpUtils(){
+      //CookieJarIml
+    }
+}
