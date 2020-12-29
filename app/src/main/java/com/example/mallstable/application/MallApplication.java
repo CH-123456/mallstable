e9a45be7aaf82627aeea875237a73b33089a7598
ytu07111 <2889653326@qq.com>
2020-12-29 11:34:32 +0800

    88

--- app/src/main/java/com/example/mallstable/application/MallApplication.java
+++ app/src/main/java/com/example/mallstable/application/MallApplication.java
@@ -2,12 +2,33 @@ package com.example.mallstable.application;
 
 import android.app.Application;
 
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.cookie.CookieJarImpl;
+import com.zhy.http.okhttp.cookie.store.PersistentCookieStore;
+import com.zhy.http.okhttp.log.LoggerInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
 public class MallApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
     }
+    /**
+     * 配置OKHTTP网络请求框架
+     * */
     private void initOkHttpUtils(){
-      //CookieJarImlhjhj
+        //保持Cookie
+        CookieJarImpl cookieJar = new CookieJarImpl(new
+                PersistentCookieStore(getApplicationContext()));
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cookieJar(cookieJar)
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L,TimeUnit.MILLISECONDS)
+                .addInterceptor(new LoggerInterceptor("TAG"))
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
     }
 }
