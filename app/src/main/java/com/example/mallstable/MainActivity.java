7ad98099b542175ea131d604b9de2bcf0e808955
杰仔 <976476963@qq.com>
2020-12-29 09:57:02 +0800

    测试1

--- app/src/main/java/com/example/mallstable/MainActivity.java
+++ app/src/main/java/com/example/mallstable/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//豆腐干士大夫是框架和符号
+    /**测试软件**/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
