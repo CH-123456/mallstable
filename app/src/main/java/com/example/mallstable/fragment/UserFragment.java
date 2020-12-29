55822aa609b9cc594978786cfc3ae5b66604cbbd
Karl-Ben-Lee <393003750@qq.com>
2020-12-29 17:20:27 +0800

    上传了userFragment

--- app/src/main/java/com/example/mallstable/fragment/UserFragment.java
+++ app/src/main/java/com/example/mallstable/fragment/UserFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mallstable.R;
 
@@ -15,18 +16,31 @@ import com.example.mallstable.R;
  * A simple {@link Fragment} subclass.
  */
 public class UserFragment extends Fragment {
-
+    private TextView user;
 
     public UserFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view=inflater.inflate(R.layout.fragment_user, container, false);
+        initView(view);
+        return view;
     }
+    //加载UI
+    private void initView(View view){
+        user=view.findViewById(R.id.user);
+        view
+        view.findViewById(R.id);
+        view.findViewById(R.id);
+        view.findViewById(R.id);
+        view.findViewById(R.id);
+        view.findViewById(R.id);
+        view.findViewById(R.id);
+        view.findViewById(R.id);
 
+    }
 }
