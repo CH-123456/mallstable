0a941d0b892a54f96610515af81b1edf1bc2e571
qwe40021314 <948632634@qq.com>
2020-12-30 16:57:12 +0800

    默认的

--- app/src/main/java/com/example/mallstable/adapter/HomeHotProductAdapter.java
+++ app/src/main/java/com/example/mallstable/adapter/HomeHotProductAdapter.java
@@ -16,6 +16,7 @@ import com.alibaba.android.vlayout.LayoutHelper;
 import com.bumptech.glide.Glide;
 import com.example.mallstable.R;
 import com.example.mallstable.config.Constant;
+import com.example.mallstable.listener.OnItemClickListener;
 import com.example.mallstable.pojo.Product;
 
 import java.util.List;
@@ -27,11 +28,16 @@ public class HomeHotProductAdapter extends DelegateAdapter.Adapter<HomeHotProduc
     private List<Product> data;
     private Context context;
     private LayoutHelper layoutHelper;
+    private OnItemClickListener onItemClickListener;
 
     public HomeHotProductAdapter(List<Product> data, Context context, LayoutHelper layoutHelper) {
         this.data = data;
         this.context = context;
         this.layoutHelper = layoutHelper;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 
     @Override
@@ -42,23 +48,31 @@ public class HomeHotProductAdapter extends DelegateAdapter.Adapter<HomeHotProduc
     @NonNull
     @Override
     public HotProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_hot_list_item,null,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_hot_list_item, null, false);
         return new HotProductViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HotProductViewHolder holder, int position) {
-        if (position == 0){
+        if (position == 0) {
             holder.titleContainer.setVisibility(View.VISIBLE);
-        }else {
+        } else {
             holder.titleContainer.setVisibility(View.GONE);
         }
         Product product = data.get(position);
         holder.name.setText(product.getName());
-        holder.price.setText("价格：¥"+product.getPrice());
-        holder.stock.setText("库存："+product.getStock());
+        holder.price.setText("价格：¥" + product.getPrice());
+        holder.stock.setText("库存：" + product.getStock());
         holder.contentContainer.setTag(position);
-        Glide.with(context).load(Constant.API.BASE_URL+product.getIconUrl()).into(holder.icon_url);
+        Glide.with(context).load(Constant.API.BASE_URL + product.getIconUrl()).into(holder.icon_url);
+        holder.contentContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(view, (int) view.getTag());
+                }
+            }
+        });
     }
 
     @Override
@@ -66,7 +80,7 @@ public class HomeHotProductAdapter extends DelegateAdapter.Adapter<HomeHotProduc
         return data.size();
     }
 
-    public static class HotProductViewHolder extends RecyclerView.ViewHolder{
+    public static class HotProductViewHolder extends RecyclerView.ViewHolder {
         public RelativeLayout titleContainer;
         public RelativeLayout contentContainer;
         public TextView btn_more;
@@ -74,15 +88,16 @@ public class HomeHotProductAdapter extends DelegateAdapter.Adapter<HomeHotProduc
         public ImageView icon_url;
         public TextView stock;
         public TextView price;
+
         public HotProductViewHolder(@NonNull View itemView) {
             super(itemView);
-            titleContainer = (RelativeLayout)itemView.findViewById(R.id.title_container);
-            contentContainer = (RelativeLayout)itemView.findViewById(R.id.content);
-            name = (TextView)itemView.findViewById(R.id.name);
-            stock = (TextView)itemView.findViewById(R.id.stock);
-            price = (TextView)itemView.findViewById(R.id.price);
-            icon_url = (ImageView)itemView.findViewById(R.id.icon_url);
-            btn_more = (TextView)itemView.findViewById(R.id.btn_more);
+            titleContainer = (RelativeLayout) itemView.findViewById(R.id.title_container);
+            contentContainer = (RelativeLayout) itemView.findViewById(R.id.content);
+            name = (TextView) itemView.findViewById(R.id.name);
+            stock = (TextView) itemView.findViewById(R.id.stock);
+            price = (TextView) itemView.findViewById(R.id.price);
+            icon_url = (ImageView) itemView.findViewById(R.id.icon_url);
+            btn_more = (TextView) itemView.findViewById(R.id.btn_more);
         }
     }
 }
--- app/src/main/java/com/example/mallstable/fragment/HomeFragment.java
+++ app/src/main/java/com/example/mallstable/fragment/HomeFragment.java
@@ -22,6 +22,7 @@ import com.example.mallstable.adapter.HomeActAdapter;
 import com.example.mallstable.adapter.HomeHotProductAdapter;
 import com.example.mallstable.adapter.HomeTopBannerAndParamAdapter;
 import com.example.mallstable.config.Constant;
+import com.example.mallstable.listener.OnItemClickListener;
 import com.example.mallstable.pojo.Param;
 import com.example.mallstable.pojo.Product;
 import com.example.mallstable.pojo.ResponeCode;
@@ -78,9 +79,9 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    private void initView(View view){
+    private void initView(View view) {
         //获取RecyerView
-        mRecylerView = (RecyclerView)view.findViewById(R.id.rv);
+        mRecylerView = (RecyclerView) view.findViewById(R.id.rv);
         //初始化轮播图
         images = new ArrayList<>();
         images.add(R.mipmap.lunbo01);
@@ -97,37 +98,42 @@ public class HomeFragment extends Fragment {
         List<DelegateAdapter.Adapter> adapters = new LinkedList<>();
 
         /*轮播图*/
-        View headView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home_banner,null,false);
-        Banner banner = (Banner)headView.findViewById(R.id.banner);
-        banner.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Utils.getScreenHeight(getActivity())/4));
+        View headView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home_banner, null, false);
+        Banner banner = (Banner) headView.findViewById(R.id.banner);
+        banner.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Utils.getScreenHeight(getActivity()) / 4));
         //设置表格布局
         GridLayoutHelper gridLayoutHelper = new GridLayoutHelper(PARAM_ROW_COL);
-        gridLayoutHelper.setSpanSizeLookup(new GridLayoutHelper.SpanSizeLookup(){
+        gridLayoutHelper.setSpanSizeLookup(new GridLayoutHelper.SpanSizeLookup() {
             @Override
             public int getSpanSize(int position) {
-                if (position == 0){
+                if (position == 0) {
                     return PARAM_ROW_COL;
-                }else {
+                } else {
                     return 1;
                 }
             }
         });
-        homeTopBannerAndParamAdapter = new HomeTopBannerAndParamAdapter(mCategoryData,getActivity(),gridLayoutHelper);
+        homeTopBannerAndParamAdapter = new HomeTopBannerAndParamAdapter(mCategoryData, getActivity(), gridLayoutHelper);
         homeTopBannerAndParamAdapter.setHeadVioew(banner);
         adapters.add(homeTopBannerAndParamAdapter);
 
         /*活动区*/
         LinearLayoutHelper linearLayoutHelper = new LinearLayoutHelper();
-        linearLayoutHelper.setMarginBottom(Utils.dp2px(getActivity(),20));
-        adapters.add(new HomeActAdapter(getActivity(),linearLayoutHelper));
+        linearLayoutHelper.setMarginBottom(Utils.dp2px(getActivity(), 20));
+        adapters.add(new HomeActAdapter(getActivity(), linearLayoutHelper));
 
         /*热销商品*/
         LinearLayoutHelper hotLayoutHelper = new LinearLayoutHelper();
-        homeHotProductAdapter = new HomeHotProductAdapter(mProductData,getActivity(),hotLayoutHelper);
+        homeHotProductAdapter = new HomeHotProductAdapter(mProductData, getActivity(), hotLayoutHelper);
+        homeHotProductAdapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int pos) {
+                //提取产品编号并跳转
+                String id = mProductData.get(pos).getId() + "";
+            }
+        });
         adapters.add(homeHotProductAdapter);
         //点击热销商品，要跳转到详情页面
-
-
 
 
         delegateAdapter = new DelegateAdapter(layoutManager);
@@ -146,7 +152,7 @@ public class HomeFragment extends Fragment {
         banner.start();
     }
 
-    private void loadParams(){
+    private void loadParams() {
         //加载产品分类参数
         OkHttpUtils.get()
                 .url(Constant.API.CATEGORY_PARAM_URL)
@@ -159,17 +165,18 @@ public class HomeFragment extends Fragment {
 
                     @Override
                     public void onResponse(String response, int id) {
-                        final Type type = new TypeToken<SverResponse<List<Param>>>(){}.getType();
-                        SverResponse<List<Param>> result = JSONUtils.fromJson(response,type);
-                        if (result.getStatus()== ResponeCode.SUCCESS.getCode()){
-                            if (result.getData()==null)
+                        final Type type = new TypeToken<SverResponse<List<Param>>>() {
+                        }.getType();
+                        SverResponse<List<Param>> result = JSONUtils.fromJson(response, type);
+                        if (result.getStatus() == ResponeCode.SUCCESS.getCode()) {
+                            if (result.getData() == null)
                                 return;
 
-                            if (result.getData().size()%PARAM_ROW_COL==0){
+                            if (result.getData().size() % PARAM_ROW_COL == 0) {
                                 mCategoryData.addAll(result.getData());
-                            }else {
-                                int count = result.getData().size()/PARAM_ROW_COL;
-                                mCategoryData.addAll(result.getData().subList(0,count*PARAM_ROW_COL));
+                            } else {
+                                int count = result.getData().size() / PARAM_ROW_COL;
+                                mCategoryData.addAll(result.getData().subList(0, count * PARAM_ROW_COL));
                             }
                             homeTopBannerAndParamAdapter.notifyDataSetChanged();
                         }
@@ -177,10 +184,10 @@ public class HomeFragment extends Fragment {
                 });
     }
 
-    private void loadHotProducts(){
+    private void loadHotProducts() {
         OkHttpUtils.get()
                 .url(Constant.API.HOT_PRODUCT_URL)
-                .addParams("num","10")
+                .addParams("num", "10")
                 .build()
                 .execute(new StringCallback() {
                     @Override
