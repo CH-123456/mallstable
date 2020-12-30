c8ad084e4069127f9bc743e673850079f40bcee4
Karl-Ben-Lee <393003750@qq.com>
2020-12-30 10:12:33 +0800

    confirm&address类

--- app/src/main/java/com/example/mallstable/ui/ConfirmOrderActivity.java
+++ app/src/main/java/com/example/mallstable/ui/ConfirmOrderActivity.java
@@ -9,6 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mallstable.R;
+import com.example.mallstable.config.Constant;
+import com.example.mallstable.pojo.Address;
+import com.example.mallstable.pojo.ResponeCode;
+import com.example.mallstable.pojo.SverResponse;
+import com.example.mallstable.pojo.User;
+import com.example.mallstable.utils.JSONUtils;
+import com.google.gson.reflect.TypeToken;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import okhttp3.Call;
 
 /*
  *created by liben 12.30
@@ -20,11 +34,14 @@ public class ConfirmOrderActivity extends AppCompatActivity {
     private TextView addr_detail;
 
     private RecyclerView recyclerView;
+
+    private Address defaultAddr;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
-
+        initView();
+        initDefaultAdr();
     }
     private void initView(){
         name=findViewById(R.id.name);
@@ -49,4 +66,50 @@ public class ConfirmOrderActivity extends AppCompatActivity {
         });
     }
 
+    //加载地址
+    private void initDefaultAdr(){
+        OkHttpUtils.get()
+                .url(Constant.API.USER_ADDR_LIST_URL)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Type type=new TypeToken<SverResponse<List<Address>>>(){}.getType();
+                        SverResponse<List<Address>> result= JSONUtils.formJson(response,type);
+                        if(result.getStatus()== ResponeCode.SUCCESS.getCode()){
+                            if(result.getData()!=null) {
+                                //迭代器找到默认地址
+                                for (Address adr : result.getData()){
+                                    if(adr.isDfault()){
+                                        defaultAddr=adr;
+                                        break;
+                                    }
+                                }
+                                //设置默认值
+                                if(defaultAddr==null){
+                                    defaultAddr=result.getData().get(0);
+                                }
+                                name.setText(defaultAddr.getName());
+                                mobile.setText(defaultAddr.getMobile());
+                                addr_detail.setText(defaultAddr.getProvince()+" "+defaultAddr.getCity()
+                                        +" "+defaultAddr.getDistrict()+" "+defaultAddr.getAddr());
+                            }else{
+                                name.setText("");
+                                mobile.setText("");
+                                addr_detail.setText("请选择收件地址");
+                            }
+                        }
+                        else {
+                            name.setText("");
+                            mobile.setText("");
+                            addr_detail.setText("请选择收件地址");
+                        }
+                    }
+                });
+    }
 }
