ca7089098055b5e37cfacd64e40bac745d790b41
qwe40021314 <948632634@qq.com>
2020-12-31 15:37:51 +0800

    默认的

--- app/src/main/java/com/example/mallstable/utils/JSONUtils.java
+++ app/src/main/java/com/example/mallstable/utils/JSONUtils.java
@@ -1,4 +1,7 @@
 package com.example.mallstable.utils;
+/**
+ * modified by bing 12.31
+ */
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -8,21 +11,22 @@ import java.lang.reflect.Type;
 //工具类，数据转换
 public class JSONUtils {
 
-    private  static Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();//创建gson对象,转化传过来的就jVAV对象
+    private static Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();//创建gson对象,转化传过来的就jVAV对象
 
-    public static <T> T formJson(String json, Type clz){
-        return gson.fromJson(json,clz);
+    public static <T> T formJson(String json, Class<T> clz) {
+        return gson.fromJson(json, clz);
     }
+
     //fan xing fang fa
-    public static <T> T fromJson(String json, Type type ){
-        return gson.fromJson(json,type);
+    public static <T> T fromJson(String json, Type type) {
+        return gson.fromJson(json, type);
     }
 
-    public static  String toJSON(Object obj){
+    public static String toJSON(Object obj) {
         return gson.toJson(obj);
     }
 
-    public static Gson getGson(){
-        return  gson;
+    public static Gson getGson() {
+        return gson;
     }
 }
