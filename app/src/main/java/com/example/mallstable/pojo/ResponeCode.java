f506099950c44bdf22d97984a275d05caa532d25
qwe40021314 <948632634@qq.com>
2020-12-31 15:26:43 +0800

    默认的

--- app/src/main/java/com/example/mallstable/pojo/ResponeCode.java
+++ app/src/main/java/com/example/mallstable/pojo/ResponeCode.java
@@ -2,9 +2,8 @@ package com.example.mallstable.pojo;
 
 public enum ResponeCode {
     //对应服务器返回的状态码，0，1
-    SUCCESS(0,"SUCESS") ,
-    ERROR(1,"ERROR")
-    ;
+    SUCCESS(0, "SUCESS"),
+    ERROR(1, "ERROR");
     private final int code;//码
     private final String desc;//描述
 
@@ -16,10 +15,8 @@ public enum ResponeCode {
         return desc;
     }
 
-    private ResponeCode(int code, String desc){
+    private ResponeCode(int code, String desc) {
         this.code = code;
         this.desc = desc;
-
-
     }
 }
