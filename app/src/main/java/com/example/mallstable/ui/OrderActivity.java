4f594c307b6fd32a7cc4336cf3fc0ff05c7b943b
杰仔 <976476963@qq.com>
2021-01-01 15:10:16 +0800

    个人全部订单查询

---
+++ app/src/main/java/com/example/mallstable/ui/OrderActivity.java
@@ -0,0 +1,20 @@
+package com.example.mallstable.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mallstable.R;
+
+/**
+ * created by zangjie 1.1
+ */
+public class OrderActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
